a6e8022786c7b25f2932183d022ebac8e8052dcf
Ali Sheikh <ali.sheikh@flexport.com>
2020-06-11 23:34:55 -0700

    added env variables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// code away!
+require('dotenv').config(); //required 
 const server = require('./server.js');
 
-
 //make the port be assiged by the Server
+//Heroku will use this value and assign itself.
 const PORT = process.env.PORT || 5050;
 
 server.listen(PORT, () => {
